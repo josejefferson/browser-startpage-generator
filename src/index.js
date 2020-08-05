e5929134745c856f844805dacd5f9d5151c0d34a
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2020-08-05 14:42:38 -0300

    ESLint

--- src/index.js
+++ src/index.js
@@ -1,98 +1,62 @@
-const bgColor = Pickr.create({
-	el: '#bg-color-picker',
-	theme: 'nano',
-	default: '#1C1C1C',
-	comparison: true,
-	components: {
-		preview: true,
-		hue: true,
-		interaction: {
-			input: true,
-			save: true
-		}
-	},
-	strings: {
-		save: 'Salvar'
-	}
-});
-
-const textColor = Pickr.create({
-	el: '#text-color-picker',
-	theme: 'nano',
-	default: '#FFFFFF',
-	comparison: true,
-	components: {
-		preview: true,
-		hue: true,
-		interaction: {
-			input: true,
-			save: true
-		}
-	},
-	strings: {
-		save: 'Salvar'
-	}
-});
-
 $('.app-add').click(() => {
-	$('.app-add').before(appModel);
-});
+	$('.app-add').before(appModel)
+})
 
 $('.apps').on('click', '.app-delete', function () {
-	$(this).parent().parent().parent().remove();
-});
+	$(this).parent().parent().parent().remove()
+})
 
 $('.app-done').click(() => {
-	$('.apps').addClass('was-validated');
+	$('.apps').addClass('was-validated')
 
-	let valid = true;
+	let valid = true
 	$('.apps input').each(function () {
 		if ($(this).is(':invalid')) {
-			valid = false;
+			valid = false
 		}
-	});
+	})
 
-	if (valid) makeStartPage();
-});
+	if (valid) makeStartPage()
+})
 
 $('.apps').on('change', '.app-picture', function (e) {
-	var fileName = e.target.files[0] ? e.target.files[0].name : 'Imagem...';
-	$(this).next('.custom-file-label').html(fileName);
-});
+	var fileName = e.target.files[0] ? e.target.files[0].name : 'Imagem...'
+	$(this).next('.custom-file-label').html(fileName)
+})
 
 function makeStartPage() {
-	let zip = new JSZip();
+	let zip = new JSZip()
 	let apps = []
-	let i = 0;
+	let i = 0
 
 	$('.app-item').each(function () {
 		let app = {}
-		app.name = $(this).find('.app-name').val();
-		app.url = $(this).find('.app-url').val();
-		let file = $(this).find('.app-picture')[0].files[0];
-		let iconName = file.name.split('.');
-		let iconExtension = iconName[iconName.length - 1];
-		app.icon = `icons/${i}.${iconExtension}`;
-		apps.push(app);
+		app.name = $(this).find('.app-name').val()
+		app.url = $(this).find('.app-url').val()
+		const file = $(this).find('.app-picture')[0].files[0]
+		const iconName = file.name.split('.')
+		const iconExtension = iconName[iconName.length - 1]
+		app.icon = `icons/${i}.${iconExtension}`
+		apps.push(app)
 
-		zip.file(`icons/${i++}.${iconExtension}`, file);
-	});
+		zip.file(`icons/${i++}.${iconExtension}`, file)
+	})
 
-	let appsGen = `var apps = ${JSON.stringify(apps)}`;
-	let bg = bgColor.getColor().toHEXA().toString();
-	let tcolor = textColor.getColor().toHEXA().toString();
+	const appsGen = `var apps = ${JSON.stringify(apps)}`
+	const bg = $('#bgColor').val()
+	const tcolor = $('#textColor').val()
 
-	zip.file('index.html', generateHTML(appsGen, bg, tcolor));
+	zip.file('index.html', generateHTML(appsGen, bg, tcolor))
 	zip.generateAsync({ type: 'blob' })
 		.then(content => {
-			let url = window.URL.createObjectURL(content);
-			$('.download').attr({ 'href': url, 'download': 'sites-launcher.zip' }).removeClass('hidden');
+			const url = window.URL.createObjectURL(content)
+			$('.download').attr({ 'href': url, 'download': 'sites-launcher.zip' }).removeClass('hidden')
 
-			var dlink = document.createElement('a');
-			document.body.appendChild(dlink);
-			dlink.style = 'display: none';
-			dlink.href = url;
-			dlink.download = 'sites-launcher.zip';
-			dlink.click();
-		});
+			let dlink = document.createElement('a')
+			document.body.appendChild(dlink)
+			dlink.style = 'display: none'
+			dlink.href = url
+			dlink.download = 'browser-launcher.zip'
+			dlink.click()
+		})
 }
